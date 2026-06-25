06363a52b11254674c34456d411e7e6b6d17e143
Toy085 <brightabaka1@gmail.com>
2026-06-25 21:24:11 +0200

    Change player speed default

--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-@export  var speed : float = 50
+@export var speed : float = 150
 
 func _physics_process(delta:float) -> void:
 	var input_vector = Input.get_vector("left", "right", "up", "down")
-	velocity = input_vector * speed 
+	velocity = input_vector * speed
 	move_and_slide()
