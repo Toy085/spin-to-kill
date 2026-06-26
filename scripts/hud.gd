82a98096f7ee1dd68dbde5b86a4f410081e1a097
Toy085 <brightabaka1@gmail.com>
2026-06-26 21:25:04 +0200

    Add healthbar

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,12 +1,14 @@
 extends Control
 
 @onready var coinlabel: RichTextLabel = $CoinLabel
+@onready var healthbar: ProgressBar = $HealthBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	healthbar.max_value = Global.health
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	coinlabel.text = "COINS: [outline_size=6][color=gold]" + str(Global.current_run_money) + "[/color][/outline_size]"
+	healthbar.value = Global.health
