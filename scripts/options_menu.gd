337c69b74f384e35048340d3d93de75c3cacb900
Toy085 <brightabaka1@gmail.com>
2026-07-06 15:07:50 +0200

    Add fullscreen toggle to options menu

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -3,14 +3,10 @@ extends Panel
 @onready var options: Panel = $"."
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _on_back_button_pressed() -> void:
 	options.visible = false
+
+
+func _on_fullscreen_toggle_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
