ea14fd95baf2749e1d5420b263880d67ca42eded
Toy085 <brightabaka1@gmail.com>
2026-07-06 13:51:15 +0200

    Add options menu to main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,8 +2,14 @@ extends Control
 
 @export var game_scene_path : String = "res://world.tscn"
 
+@onready var options: Panel = $Options
+
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file(game_scene_path)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_button_pressed() -> void:
+	options.visible = true
