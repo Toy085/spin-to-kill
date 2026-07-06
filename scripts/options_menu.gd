3476d557b0359808947d9fd908d9615b82126402
Toy085 <brightabaka1@gmail.com>
2026-07-06 15:11:38 +0200

    Add fullscreen toggle to options

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -2,11 +2,12 @@ extends Panel
 
 @onready var options: Panel = $"."
 
-
-
 func _on_back_button_pressed() -> void:
 	options.visible = false
 
 
 func _on_fullscreen_toggle_toggled(toggled_on: bool) -> void:
-	pass # Replace with function body.
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
