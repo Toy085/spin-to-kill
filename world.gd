bdaec7ae50b54a7029069c11a5dea55847059acc
Toy085 <brightabaka1@gmail.com>
2026-07-08 23:05:46 +0200

    Pause player while in

--- world.gd
+++ world.gd
@@ -11,8 +11,10 @@ func _ready() -> void:
 		return
 		
 	player.visible = false
+	player.set_physics_process(false)
 	DialogueManager.show_example_dialogue_balloon(crash_dialogue, "start")
 	
 	await DialogueManager.dialogue_ended
 	Global.crash_scene = true
 	player.visible = true
+	player.set_physics_process(true)
