121e1b648677520bda1df6a4596640eb4e76bff7
Toy085 <brightabaka1@gmail.com>
2026-07-08 07:56:06 +0200

    Add enemy knockback

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,6 +4,7 @@ class_name Enemy
 
 @export var speed : float = 50
 @export var attack_damage: int = 1
+@export var knockback_strength = 350
 
 @onready var coin: PackedScene = preload("res://coin.tscn")
 @onready var attack_cooldown: Timer = $AttackCooldown
@@ -12,6 +13,8 @@ var player_in_range: CharacterBody2D = null
 
 var player: CharacterBody2D
 var health = randi_range(5, 20)
+
+var knockback_velocity: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -22,11 +25,14 @@ func _process(delta: float) -> void:
 
 	if player:
 		var direction = (player.global_position - global_position).normalized()
-		velocity = direction * speed
+		velocity = direction * speed + knockback_velocity
 		move_and_slide()
+		knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, 500 * delta)
 
-func damage(damage: int) -> void:
+func damage(damage: int, dir: Vector2) -> void:
 	health -= damage
+	
+	knockback_velocity = dir * knockback_strength
 
 func die() -> void:
 	var reward = randi_range(1 + Global.greed, 5 + Global.greed)
