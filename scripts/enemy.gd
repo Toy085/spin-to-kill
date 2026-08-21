53f956694e2d473923b6a804af3f0cc695219ea9
Toy085 <brightabaka1@gmail.com>
2026-08-21 13:47:12 +0200

    Fix enemy speed

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -31,11 +31,11 @@ func _ready() -> void:
 			animated_sprite_2d.play("Enemy1")
 		else:
 			animated_sprite_2d.play("Enemy3")
-			speed = speed * 1.5
 			health = randi_range(1, 5)
 			var clone = self.duplicate() as Enemy
 			clone.global_position = global_position + Vector2(20, 0)
 			get_parent().add_child(clone)
+			speed = speed * 1.5
 	elif rng < 1:
 		enemy_type = Type.fly
 		speed = speed * 1.25
