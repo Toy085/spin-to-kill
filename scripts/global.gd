61729314bb5b422b95274fb586d5e0bf0d768ebc
Toy085 <brightabaka1@gmail.com>
2026-08-21 20:25:45 +0200

    Add death counter in global.gd

--- scripts/global.gd
+++ scripts/global.gd
@@ -11,6 +11,7 @@ var damage: int = 5
 var greed: int = 0
 var coin_radius: float = 32
 var axe_range: float = 50
+var deaths: int = 0
 
 var crash_scene: bool = false
 
@@ -34,5 +35,6 @@ func save_item_level(item_name: String, level: int) -> void:
 func player_died():
 	total_money += current_run_money
 	current_run_money = 0
+	deaths += 1
 	
 	get_tree().call_deferred("change_scene_to_file", "res://ShopMenu.tscn")
