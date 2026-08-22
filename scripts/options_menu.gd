03df3a3ef2e6744c47f2fa8487c5b63081535c65
Toy085 <brightabaka1@gmail.com>
2026-08-22 15:41:08 +0200

    Add language dropdown to options menu

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -3,6 +3,32 @@ extends Panel
 
 @onready var options: Panel = $"."
 @onready var audio_control: HSlider = $VBoxContainer/MasterSliderLabel/AudioControl
+@onready var lang_dropdown: OptionButton = $VBoxContainer/LangLabel/LangDropdown
+
+var locales: PackedStringArray = []
+
+func _ready() -> void:
+	_setup_language_dropdown()
+
+func _setup_language_dropdown() -> void:
+	lang_dropdown.clear()
+	
+	locales = TranslationServer.get_loaded_locales()
+
+	var active_locale: String = TranslationServer.get_locale()
+	var select_index: int = 0
+
+	for i in range(locales.size()):
+		var locale_code: String = locales[i]
+		var language_name: String = TranslationServer.get_locale_name(locale_code)
+		
+		lang_dropdown.add_item(language_name)
+
+		if active_locale.begins_with(locale_code):
+			select_index = i
+
+	if locales.size() > 0:
+		lang_dropdown.select(select_index)
 
 func _on_back_button_pressed() -> void:
 	options.visible = false
@@ -15,3 +41,7 @@ func _on_fullscreen_toggle_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_lang_dropdown_item_selected(index: int) -> void:
+	var selected_locale: String = locales[index]
+	TranslationServer.set_locale(selected_locale)
