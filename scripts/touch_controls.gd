04da1fbf21f7c981adfc94f49fc5756cceaf22d9
Toy085 <brightabaka1@gmail.com>
2026-08-23 11:44:03 +0200

    Turn on touch controls when touch is detected

--- scripts/touch_controls.gd
+++ scripts/touch_controls.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _ready() -> void:
 	visible = Global.is_mobile_os
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch or event is InputEventScreenDrag:
+		visible = true
