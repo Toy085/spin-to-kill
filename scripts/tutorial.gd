5ba4e7a2626fdf55382d88114acdd6e77bcae696
Toy085 <brightabaka1@gmail.com>
2026-08-25 20:46:32 +0200

    Update tutorail with player pausing and waiting for animation

--- scripts/tutorial.gd
+++ scripts/tutorial.gd
@@ -9,6 +9,8 @@ var tut = self
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.set_physics_process(false)
 	
 	DialogueManager.show_example_dialogue_balloon(tutorial_dialouge, "start", [self])
 	await DialogueManager.dialogue_ended
@@ -16,24 +18,40 @@ func _ready() -> void:
 func wait_for_player_distance(target_distance: float) -> void:
 	if not player:
 		return
-	
+		
+	player.set_physics_process(true)
+
 	var start_pos: Vector2 = player.global_position
 	
 	while player.global_position.distance_to(start_pos) < target_distance:
 		await get_tree().process_frame
+	
+	player.set_physics_process(false)
+
 
 func wait_for_player_spin_attack() -> void:
 	if not player:
 		return
-	
+		
+	player.set_physics_process(true)
+
 	while not player.is_spinning:
 		await get_tree().process_frame
+	
+	await get_tree().create_timer(0.75).timeout
+	player.set_physics_process(false)
 
 func wait_for_player_throw() -> void:
 	if not player:
 		return
 	
+	player.set_physics_process(true)
+
 	await player.axe_thrown
+	await get_tree().create_timer(0.75).timeout
+	
+	player.set_physics_process(false)
+
 
 func quit() -> void:
 	Global.done_tutorial = true
